08fd129f49dc2269f3c1b03572259a27a91322f1
Chad <wehbe.chady@gmail.com>
2022-03-12 17:03:44 +0100

    Added submenus (mobile) and minor improvements.

--- themes/dissent/static/js/scripts.js
+++ themes/dissent/static/js/scripts.js
@@ -1,14 +1,14 @@
-// The spacing of the submenus (in pixels).
+// The spacing of the submenus for screen (in pixels).
 let menuTopSpacing = 24;
 let menuLeftSpacing = 0;
 
-const countDownDate = new Date("Aug 20, 2022 15:37:25").getTime();
+const countDownDate = new Date("Aug 20, 2022").getTime();
 
 let now = new Date().getTime();
 let timeDifference = countDownDate - now;
 
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("days_counter").innerHTML = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    document.getElementById("days_counter").innerHTML = Math.floor(timeDifference / (1000 * 60 * 60 * 24)).toString();
 
     // let menuBackground = document.getElementById("header_background");
     // let mainImageContainer = document.getElementById("main_image_container");
@@ -22,19 +22,36 @@ document.addEventListener("DOMContentLoaded", function(){
 
     document.getElementById("hamburger_menu_button").addEventListener("click", toggleMenuVisibility);
 
-    document.body.addEventListener("click", resetSubmenu);
+    document.body.addEventListener("click", resetSubmenus);
+    // document.body.addEventListener("click", resetMobileSubmenus);
 
     let menuItems = document.getElementsByClassName("menu-item");
+    let mobileMenuItems = document.getElementsByClassName("hamburger-menu-item");
 
     for (let i = 0; i < menuItems.length; i++) {
-        menuItems[i].addEventListener("mouseover", showSubmenu, false);
-        // menuItems[i].addEventListener("mouseout", resetSubmenu, false);
+        let submenu = menuItems[i].getAttribute("data-submenu");
+
+        // The menu has a submenu.
+        if (submenu && submenu !== "none") {
+            menuItems[i].addEventListener("mouseover", showSubmenu, false);
+        }
     }
 
+    for (let i = 0; i < mobileMenuItems.length; i++) {
+        let submenu = mobileMenuItems[i].getAttribute("data-submenu");
+
+        // The menu has a submenu.
+        if (submenu && submenu !== "none") {
+            mobileMenuItems[i].addEventListener("click", toggleMobileSubmenu, false);
+        }
+    }
+
+        // menuItems[i].addEventListener("mouseout", resetSubmenus, false);
 });
 
-function showSubmenu () {
-    resetSubmenu();
+function showSubmenu ()
+{
+    resetSubmenus();
 
     let submenuId = this.getAttribute("data-submenu");
 
@@ -48,18 +65,48 @@ function showSubmenu () {
     }
 }
 
-function resetSubmenu () {
+function toggleMobileSubmenu()
+{
+    resetMobileSubmenus();
+
+    let submenuId = this.getAttribute("data-submenu");
+
+    if (submenuId !== "none") {
+        let submenu = document.getElementById(submenuId);
+
+        if (submenu.classList.contains("mobile-submenu-open")) {
+            submenu.classList.replace("mobile-submenu-open", "mobile-submenu-closed");
+        } else {
+            submenu.classList.replace("mobile-submenu-closed", "mobile-submenu-open");
+        }
+    }
+}
+
+function resetSubmenus ()
+{
     let submenuItems = document.getElementsByClassName("submenu");
+
     for (let i = 0; i < submenuItems.length; i++) {
         submenuItems[i].style.display = "none";
     }
 }
 
-function toggleMenuVisibility () {
+function resetMobileSubmenus ()
+{
+    let mobileSubmenuItems  = document.getElementsByClassName("hamburger-submenu");
+
+    for (let i = 0; i < mobileSubmenuItems.length; i++) {
+        mobileSubmenuItems[i].classList.replace("mobile-submenu-open", "mobile-submenu-closed");
+    }
+}
+
+function toggleMenuVisibility ()
+{
     toggleVisibility(document.getElementById("hamburger_menu_window"));
 }
 
-function toggleVisibility (element) {
+function toggleVisibility (element)
+{
     if (element.style.display === "") {
         element.style.display = "block";
     } else {
