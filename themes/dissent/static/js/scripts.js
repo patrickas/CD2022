f7c69edd07e9040bc6b18070f2c134d0f45b76aa
Chad <wehbe.chady@gmail.com>
2022-03-13 12:43:50 +0100

    Added "About" page.

--- themes/dissent/static/js/scripts.js
+++ themes/dissent/static/js/scripts.js
@@ -8,7 +8,9 @@ let now = new Date().getTime();
 let timeDifference = countDownDate - now;
 
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("days_counter").innerHTML = Math.floor(timeDifference / (1000 * 60 * 60 * 24)).toString();
+    if (document.getElementById("days_counter")) {
+        document.getElementById("days_counter").innerHTML = Math.floor(timeDifference / (1000 * 60 * 60 * 24)).toString();
+    }
 
     // let menuBackground = document.getElementById("header_background");
     // let mainImageContainer = document.getElementById("main_image_container");
