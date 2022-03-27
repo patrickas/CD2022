0c7ffab78aa6293626cc1eb74c7ce6918c40a09c
Patrick Abi Salloum <patrick.abisalloum@gmail.com>
2022-03-27 21:15:52 +0300

    make the menu sticky in the homepage

--- themes/dissent/static/js/scripts.js
+++ themes/dissent/static/js/scripts.js
@@ -2,11 +2,31 @@ const countDownDate = new Date("Aug 20, 2022").getTime();
 
 let now = new Date().getTime();
 let timeDifference = countDownDate - now;
+var sticky;
+var header ;
 
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function sticky_add_remove() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 document.addEventListener("DOMContentLoaded", function(){
     if (document.getElementById("days_counter")) {
         document.getElementById("days_counter").innerHTML = Math.floor(timeDifference / (1000 * 60 * 60 * 24)).toString();
     }
+
+	// When the user scrolls the page, execute myFunction
+	window.onscroll = function() {sticky_add_remove()};
+
+	// Get the header
+	header = document.getElementById("sticky-menu-desktop");
+
+	// Get the offset position of the navbar
+	sticky = header.offsetTop;
+
 });
 
 const isVisible = elem => !!elem && !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length ) // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.j
